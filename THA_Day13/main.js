90915aae5a64c50345ba78880026099f21dc6dc1
Debee-Prasad <63064795+Debee-Prasad@users.noreply.github.com>
2021-07-12 00:35:58 +0530

    some changes

---
+++ THA_Day13/main.js
@@ -0,0 +1,6 @@
+let squares = document.getElementsByClassName('baksa');
+
+Array.prototype.forEach.call(squares, (el)=>{
+el.addEventListener('contextmenu', (event)=>{
+    el.classList.toggle('active');})
+});
